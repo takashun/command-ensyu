489074f40be393da4ecf8e991de71f95053b88d4
takahashi <shu.takahashi.sys22@morijyobi.ac.jp>
2022-11-10 10:13:49 +0900

    Main.javaの仕様を更新

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main{
 public static void main(String[] args){
+	for(int i=1;i<=2;i++){
+	System.out.println(i+"人目");
 	System.out.print("身長(m):");
 	double height = new java.util.Scanner(System.in).nextDouble();
 	System.out.print("体重(kg):");
@@ -8,4 +10,5 @@ public static void main(String[] args){
 	double bmi = weight / (height * height);
 	System.out.println(String.format("%.2f",bmi));
 	}
+}
 }
